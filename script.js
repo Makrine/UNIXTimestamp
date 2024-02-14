fd45cdc9585c39fdf3b40b96dc17cb4ffc5fa5a0
Makrina Tskipurishvili <66353680+Makrine@users.noreply.github.com>
2024-02-14 13:56:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// Function to convert a number to binary string representation
 function toBinary(num) {
-    let binaryString = num.toString(2).padStart(32, '0'); // Pad to ensure 32 bits
-    let formattedBinary = ''; // Initialize empty string for formatted binary
-
-    // Loop through the binary string
+    let binaryString = num.toString(2).padStart(32, '0');
+    let formattedBinary = '';
     for (let i = 0; i < binaryString.length; i++) {
-        // Add the current character to the formatted binary string
         formattedBinary += binaryString[i];
-        // Insert a space after every 8 bits
         if ((i + 1) % 8 === 0 && i !== binaryString.length - 1) {
             formattedBinary += ' ';
         }
@@ -16,8 +11,6 @@ function toBinary(num) {
     return formattedBinary;
 }
 
-
-// Function to update the display with the current binary representation
 function updateDisplay(binaryRepresentation, decimal) {
     document.getElementById('binaryDisplay').textContent = binaryRepresentation;
     document.getElementById('decimalDisplay').textContent = decimal;
@@ -28,15 +21,11 @@ const startDate = new Date('July 28, 2000 17:30:00');
 const currentTime = new Date();
 const elapsedTimeInSeconds = (currentTime - startDate) / 1000;
 
-// Calculate the initial value of the 32-bit variable based on elapsed time
 let num = Math.floor(elapsedTimeInSeconds);
 
-// Output the initial binary representation
-updateDisplay(toBinary(num));
+updateDisplay(toBinary(num), num);
 
-// Set interval to update the display every one second (1000 milliseconds)
 setInterval(function() {
-    // Update the variable and display its binary representation
     num++;
     updateDisplay(toBinary(num), num);
 }, 1000);
